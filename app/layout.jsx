d8302a3d3207531b442c291c8f0a48f9e1d28a65
nicoegiaian <nicolas.egiaian@gmail.com>
2025-08-18 23:50:57 -0300

    Add Google Analytics tag to site

--- app/layout.jsx
+++ app/layout.jsx
@@ -26,6 +26,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TLTZMFB49W"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TLTZMFB49W');
+            `,
+          }}
+        />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
